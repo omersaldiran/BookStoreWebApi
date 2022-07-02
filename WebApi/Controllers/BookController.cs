c5bc38316ee222340c09e7c0023feb05baff9d70
omersaldiran <osaldiran@gmail.com>
2022-07-02 19:48:29 +0300

    CRUD işlemleri eklendi

--- WebApi/Controllers/BookController.cs
+++ WebApi/Controllers/BookController.cs
@@ -28,5 +28,47 @@ namespace WebApi.AddControllers{
                 PublishDate = new DateTime(2001,12,21) 
             }
         };
+
+        [HttpGet]
+        public List<Book> GetBooks(){
+            var bookList = BookList.OrderBy(x => x.Id).ToList<Book>();
+            return bookList;
+        }
+
+         [HttpGet("{id}")]
+        public Book GetById(int id){
+            var book = BookList.Where(book=> book.Id == id).SingleOrDefault();
+            return book;
+        }
+
+        //  [HttpGet]
+        // public Book Get([FromQuery] string id){
+        //     var book = BookList.Where(book=> book.Id == Convert.ToInt32(id)).SingleOrDefault();
+        //     return book;
+        // }
+
+        [HttpPost]
+        public IActionResult AddBook([FromBody] Book newBook){
+            var book = BookList.SingleOrDefault(x => x.Title == newBook.Title);
+            if(book is not null){
+                return BadRequest();
+            }
+
+            BookList.Add(newBook);
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateBook(int id, [FromBody] Book updatedBook){
+            var book = BookList.SingleOrDefault(x => x.Id == id);
+            if(book is null){
+                return BadRequest();
+            }
+            book.GenreId = updatedBook.GenreId != default ? updatedBook.GenreId : book.GenreId;
+            book.PageCount = updatedBook.PageCount != default ? updatedBook.PageCount : book.PageCount;
+            book.Title = updatedBook.Title != default ? updatedBook.Title : book.Title;
+            book.PublishDate = updatedBook.PublishDate != default ? updatedBook.PublishDate : book.PublishDate;
+            return Ok();
+        }
     }
 }
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -13,7 +13,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+            {
+                c.RoutePrefix = string.Empty;
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "MY API");
+            });
 }
 
 app.UseHttpsRedirection();
