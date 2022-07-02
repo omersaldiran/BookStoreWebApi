a70eed4c735d492c4ebad827bfd818b6d37c1c5f
omersaldiran <osaldiran@gmail.com>
2022-07-02 12:38:56 +0300

    first commit

---
+++ WebApi/Controllers/BookController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.AddControllers{
+
+    [ApiController]
+    [Route("[controller]s")]
+    public class BookController : ControllerBase{
+        
+    }
+}
