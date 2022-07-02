4eded38d398354763fe382299680f32203dd3ecf
omersaldiran <osaldiran@gmail.com>
2022-07-02 12:58:34 +0300

    Controller eklendi

--- WebApi/Controllers/BookController.cs
+++ WebApi/Controllers/BookController.cs
@@ -5,6 +5,28 @@ namespace WebApi.AddControllers{
     [ApiController]
     [Route("[controller]s")]
     public class BookController : ControllerBase{
-        
+        public static List<Book> BookList = new List<Book>(){
+            new Book{
+                Id = 1,
+                Title = "Lean Startup",
+                GenreId = 1, // Personel Growth
+                PageCount = 200,
+                PublishDate = new DateTime(2001,6,12) 
+            },
+            new Book{
+                Id = 2,
+                Title = "Herland",
+                GenreId = 2, // Science Fiction
+                PageCount = 250,
+                PublishDate = new DateTime(2010,5,23) 
+            },
+            new Book{
+                Id = 3,
+                Title = "Dune",
+                GenreId = 2, // Personel Growth
+                PageCount = 540,
+                PublishDate = new DateTime(2001,12,21) 
+            }
+        };
     }
 }
