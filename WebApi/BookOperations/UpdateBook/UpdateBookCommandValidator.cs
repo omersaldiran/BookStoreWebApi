8e8555994c94fde64e2e19a4a7c85c901aa53e18
omersaldiran <osaldiran@gmail.com>
2022-07-14 12:52:45 +0300

    UpdateBook Validetor güncellendi

--- WebApi/BookOperations/UpdateBook/UpdateBookCommandValidator.cs
+++ WebApi/BookOperations/UpdateBook/UpdateBookCommandValidator.cs
@@ -7,6 +7,8 @@ namespace WebApi.BookOperations.UpdateBook
     public class UpdateBookCommandValidator : AbstractValidator<UpdateBookCommand>{
         public UpdateBookCommandValidator(){
             RuleFor(command => command.BookId).GreaterThan(0);
+            RuleFor(command => command.Model.GenreId).GreaterThan(0);
+            RuleFor(command => command.Model.Title).NotEmpty();
         }
     }
 }
