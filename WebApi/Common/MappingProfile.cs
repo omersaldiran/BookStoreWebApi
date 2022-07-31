ed837837dd28c243d15818c6a36b7e71af022b29
omersaldiran <osaldiran@gmail.com>
2022-07-31 21:37:18 +0300

    Author sınıfı,validasyonlar ve Controller eklendi

--- WebApi/Common/MappingProfile.cs
+++ WebApi/Common/MappingProfile.cs
@@ -1,4 +1,7 @@
 using AutoMapper;
+using WebApi.Application.AuthorOperations.Commands.CreateAuthor;
+using WebApi.Application.AuthorOperations.Queries.GetAuthorDetail;
+using WebApi.Application.AuthorOperations.Queries.GetAuthors;
 using WebApi.Application.BookOperations.Queries.GetBookDetail;
 using WebApi.Application.BookOperations.Queries.GetBooks;
 using WebApi.Application.GenreOperations.Queries.GetGenreDetail;
@@ -15,6 +18,9 @@ namespace WebApi.Common
             CreateMap<Book,BooksViewModel>().ForMember(dest => dest.Genre, opt => opt.MapFrom(src => src.Genre.Name));
             CreateMap<Genre,GenresViewModel>();
             CreateMap<Genre,GenreDetailViewModel>();
+            CreateMap<Author,AuthorsViewModel>();
+            CreateMap<Author,AuthorDetailViewModel>();
+            CreateMap<CreateAuthorModel,Author>();
         }
     }
 }
--- WebApi/DBOperations/DataGenerator.cs
+++ WebApi/DBOperations/DataGenerator.cs
@@ -9,6 +9,25 @@ namespace WebApi.DBOperations{
                     return;
                 }
 
+
+                context.Authors.AddRange(
+                    new Author{
+                        Name = "Eric",
+                        Surname = "Ries",
+                        Birthday = new DateTime(1978,9,22)
+                    },
+                    new Author{
+                        Name = "Charlotte Perkins",
+                        Surname = "Gilman",
+                        Birthday = new DateTime(1860,7,3)
+                    },
+                    new Author{
+                        Name = "Frank",
+                        Surname = "Herbert",
+                        Birthday = new DateTime(1920,10,8)
+                    }
+                );
+
                 context.Genres.AddRange(
                     new Genre{
                         Name = "PersonalGrowth"
@@ -26,21 +45,24 @@ namespace WebApi.DBOperations{
                 Title = "Lean Startup",
                 GenreId = 1, 
                 PageCount = 200,
-                PublishDate = new DateTime(2001,6,12) 
+                PublishDate = new DateTime(2001,6,12),
+                AuthorId = 1
                 },
                 new Book{
                     //Id = 2,
                     Title = "Herland",
                     GenreId = 2, 
                     PageCount = 250,
-                    PublishDate = new DateTime(2010,5,23) 
+                    PublishDate = new DateTime(2010,5,23),
+                    AuthorId = 2 
                 },
                 new Book{
                     //Id = 3,
                     Title = "Dune",
                     GenreId = 2, 
                     PageCount = 540,
-                    PublishDate = new DateTime(2001,12,21) 
+                    PublishDate = new DateTime(2001,12,21),
+                    AuthorId = 3
                 });
 
                 context.SaveChanges();
